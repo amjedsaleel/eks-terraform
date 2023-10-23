0cc6e82725621db10198e958eaa4c71aeff3f9c7
amjedsaleel <amjedsaleel@gmail.com>
2023-10-23 14:01:05 +0530

    VPC configuration

---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "vpc_cidr_block" {
+  type        = string
+  description = "eks vpc cidr block"
+}
+
+variable "private_subnets_cidr_blocks" {
+  type        = list(any)
+  description = "eks vpc private subnet cidr blocks"
+}
+
+variable "public_subnets_cidr_blocks" {
+  type        = list(any)
+  description = "eks vpc sublic subnet cidr blocks"
+}
+
+variable "eks_cluster" {
+  type        = string
+  description = "eks cluster name"
+}
---
+++ vpc.tf
@@ -0,0 +1,32 @@
+data "aws_availability_zones" "azs" {
+  state = "available"
+}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.1.2"
+
+  name            = "eks-vpc"
+  cidr            = var.vpc_cidr_block
+  azs             = data.aws_availability_zones.azs.names
+  private_subnets = var.private_subnets_cidr_blocks
+  public_subnets  = var.public_subnets_cidr_blocks
+
+  enable_nat_gateway   = true
+  single_nat_gateway   = false
+  enable_dns_hostnames = true
+
+  tags = {
+    "kubernetes.io/cluster/${var.eks_cluster}" = "shared"
+    "Env"                                  = "Prod"
+    "Terraform"                            = "true"
+  }
+
+  public_subnet_tags = {
+    "kubernetes.io/role/elb" = "1"
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/role/internal-elb" = "1"
+  }
+}
